28a6628cf352eb76967a7f968a3ca5e8e3af0d56
himanshu-opkey <142556438+himanshu-opkey@users.noreply.github.com>
2025-12-16 19:09:57 +0530

    fix issue embedded
    This reverts commit

--- superset/charts/filters.py
+++ superset/charts/filters.py
@@ -14,9 +14,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 from typing import Any
 
 from flask_babel import lazy_gettext as _
+from flask_login import current_user
 from sqlalchemy import and_, or_
 from sqlalchemy.orm import aliased
 from sqlalchemy.orm.query import Query
@@ -31,17 +33,16 @@ from superset.utils.core import get_user_id
 from superset.utils.filters import get_dataset_access_filters
 from superset.views.base import BaseFilter
 from superset.views.base_api import BaseFavoriteFilter
-from flask_login import current_user
-from sqlalchemy import or_
 
 
-class ChartAllTextFilter(BaseFilter):  # pylint: disable=too-few-public-methods
+class ChartAllTextFilter(BaseFilter):
     name = _("All Text")
     arg_name = "chart_all_text"
 
     def apply(self, query: Query, value: Any) -> Query:
         if not value:
             return query
+
         ilike_value = f"%{value}%"
         return query.filter(
             or_(
@@ -53,165 +54,167 @@ class ChartAllTextFilter(BaseFilter):  # pylint: disable=too-few-public-methods
         )
 
 
-class ChartFavoriteFilter(BaseFavoriteFilter):  # pylint: disable=too-few-public-methods
-    """
-    Custom filter for the GET list that filters all charts that a user has favored
-    """
-
+# -------------------------
+# FAVORITES
+# -------------------------
+class ChartFavoriteFilter(BaseFavoriteFilter):
     arg_name = "chart_is_favorite"
     class_name = "slice"
     model = Slice
 
 
-class ChartTagNameFilter(BaseTagNameFilter):  # pylint: disable=too-few-public-methods
-    """
-    Custom filter for the GET list that filters all charts associated with
-    a certain tag (by its name).
-    """
-
+# -------------------------
+# TAG FILTERS
+# -------------------------
+class ChartTagNameFilter(BaseTagNameFilter):
     arg_name = "chart_tags"
     class_name = "slice"
     model = Slice
 
 
-class ChartTagIdFilter(BaseTagIdFilter):  # pylint: disable=too-few-public-methods
-    """
-    Custom filter for the GET list that filters all charts associated with
-    a certain tag (by its ID).
-    """
-
+class ChartTagIdFilter(BaseTagIdFilter):
     arg_name = "chart_tag_id"
     class_name = "slice"
     model = Slice
 
 
-class ChartCertifiedFilter(BaseFilter):  # pylint: disable=too-few-public-methods
-    """
-    Custom filter for the GET list that filters all certified charts
-    """
-
+# -------------------------
+# CERTIFICATION
+# -------------------------
+class ChartCertifiedFilter(BaseFilter):
     name = _("Is certified")
     arg_name = "chart_is_certified"
 
     def apply(self, query: Query, value: Any) -> Query:
         if value is True:
-            return query.filter(and_(Slice.certified_by.isnot(None)))
+            return query.filter(Slice.certified_by.isnot(None))
         if value is False:
-            return query.filter(and_(Slice.certified_by.is_(None)))
+            return query.filter(Slice.certified_by.is_(None))
         return query
 
 
-
+# -------------------------
+# DATASET / CHART ACCESS
+# -------------------------
 class ChartFilter(BaseFilter):
-    model = models.Slice  # Charts
+    model = models.Slice
 
     def apply(self, query: Query, value: Any) -> Query:
-        # Admin: return everything
+        # Admin can see everything
         if security_manager.can_access_all_datasources():
             return query
 
-        #  Role-based dataset permissions
-        perms = security_manager.user_view_menu_names("datasource_access")
-        allowed_dataset_ids = []
-        for p in perms:
-            if "(id:" in p:
-                ds_id = int(p.split("(id:")[1].split(")")[0])
-                allowed_dataset_ids.append(ds_id)
+        # Guest users: rely only on dataset access rules
+        if security_manager.is_guest_user():
+            return query.filter(get_dataset_access_filters(self.model))
 
-        #  Datasets owned by the current user
+        user_id = get_user_id()
+
+        # Dataset permissions via roles
+        perms = security_manager.user_view_menu_names("datasource_access")
+        allowed_dataset_ids = [
+            int(p.split("(id:")[1].split(")")[0])
+            for p in perms
+            if "(id:" in p
+        ]
+
+        # Datasets owned by user
         owned_dataset_ids = (
             db.session.query(models.SqlaTable.id)
             .join(models.SqlaTable.owners)
-            .filter(security_manager.user_model.id == current_user.id)
+            .filter(security_manager.user_model.id == user_id)
         )
 
-        #  Charts owned by the current user
+        # Charts owned by user
         owned_chart_ids = (
             db.session.query(models.Slice.id)
             .join(models.Slice.owners)
-            .filter(security_manager.user_model.id == current_user.id)
+            .filter(security_manager.user_model.id == user_id)
         )
 
-        #  Combine dataset & chart access rules
-        filters = or_(
-            # User has access to dataset through role
-            self.model.datasource_id.in_(allowed_dataset_ids),
-
-            # User owns the dataset
-            self.model.datasource_id.in_(owned_dataset_ids),
-
-            # User owns the chart
-            self.model.id.in_(owned_chart_ids),
+        return query.filter(
+            or_(
+                self.model.datasource_id.in_(allowed_dataset_ids),
+                self.model.datasource_id.in_(owned_dataset_ids),
+                self.model.id.in_(owned_chart_ids),
+            )
         )
 
-        return query.filter(filters)
 
-
-class ChartHasCreatedByFilter(BaseFilter):  # pylint: disable=too-few-public-methods
-    """
-    Custom filter for the GET list that filters all charts created by user
-    """
-
+# -------------------------
+# CREATED BY EXISTS
+# -------------------------
+class ChartHasCreatedByFilter(BaseFilter):
     name = _("Has created by")
     arg_name = "chart_has_created_by"
 
     def apply(self, query: Query, value: Any) -> Query:
         if value is True:
-            return query.filter(and_(Slice.created_by_fk.isnot(None)))
+            return query.filter(Slice.created_by_fk.isnot(None))
         if value is False:
-            return query.filter(and_(Slice.created_by_fk.is_(None)))
+            return query.filter(Slice.created_by_fk.is_(None))
         return query
 
 
-class ChartCreatedByMeFilter(BaseFilter):  # pylint: disable=too-few-public-methods
+# -------------------------
+# CREATED / CHANGED BY ME
+# -------------------------
+class ChartCreatedByMeFilter(BaseFilter):
     name = _("Created by me")
     arg_name = "chart_created_by_me"
 
     def apply(self, query: Query, value: Any) -> Query:
+        if security_manager.is_guest_user():
+            return query
+
+        user_id = get_user_id()
         return query.filter(
             or_(
-                Slice.created_by_fk  # pylint: disable=comparison-with-callable
-                == get_user_id(),
-                Slice.changed_by_fk  # pylint: disable=comparison-with-callable
-                == get_user_id(),
+                Slice.created_by_fk == user_id,
+                Slice.changed_by_fk == user_id,
             )
         )
 
 
-class ChartOwnedCreatedFavoredByMeFilter(BaseFilter):  # pylint: disable=too-few-public-methods
-    """
-    Custom filter for the GET chart that filters all charts the user
-    owns, created, changed or favored.
-    """
-
+# -------------------------
+# OWNED / CREATED / FAVORED
+# -------------------------
+class ChartOwnedCreatedFavoredByMeFilter(BaseFilter):
     name = _("Owned Created or Favored")
     arg_name = "chart_owned_created_favored_by_me"
 
     def apply(self, query: Query, value: Any) -> Query:
-        # If anonymous user filter nothing
-        if security_manager.current_user is None:
+        # Skip for anonymous or guest users
+        if (
+            security_manager.current_user is None
+            or security_manager.is_guest_user()
+        ):
             return query
+
+        user_id = get_user_id()
 
         owner_ids_query = (
             db.session.query(Slice.id)
             .join(Slice.owners)
-            .filter(security_manager.user_model.id == get_user_id())
+            .filter(security_manager.user_model.id == user_id)
         )
 
-        return query.join(
-            FavStar,
-            and_(
-                FavStar.user_id == get_user_id(),
-                FavStar.class_name == "slice",
-                Slice.id == FavStar.obj_id,
-            ),
-            isouter=True,
-        ).filter(
-            # pylint: disable=comparison-with-callable
-            or_(
-                Slice.id.in_(owner_ids_query),
-                Slice.created_by_fk == get_user_id(),
-                Slice.changed_by_fk == get_user_id(),
-                FavStar.user_id == get_user_id(),
+        return (
+            query.join(
+                FavStar,
+                and_(
+                    FavStar.user_id == user_id,
+                    FavStar.class_name == "slice",
+                    Slice.id == FavStar.obj_id,
+                ),
+                isouter=True,
+            )
+            .filter(
+                or_(
+                    Slice.id.in_(owner_ids_query),
+                    Slice.created_by_fk == user_id,
+                    Slice.changed_by_fk == user_id,
+                    FavStar.user_id == user_id,
+                )
             )
         )
